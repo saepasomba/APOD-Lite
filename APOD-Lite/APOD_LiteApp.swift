fde670211426f09173dc9b3e8f912a60806f922b
Sae Pasomba <saepasomba12@gmail.com>
2022-04-30 14:14:53 +0700

    Initial Commit

---
+++ APOD-Lite/APOD_LiteApp.swift
@@ -0,0 +1,17 @@
+//
+//  APOD_LiteApp.swift
+//  APOD-Lite
+//
+//  Created by Sae Pasomba on 30/04/22.
+//
+
+import SwiftUI
+
+@main
+struct APOD_LiteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ APOD-Lite/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  APOD-Lite
+//
+//  Created by Sae Pasomba on 30/04/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
